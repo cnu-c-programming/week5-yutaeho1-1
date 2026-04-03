8471bef074e7052ecc189e1e9ea6a7f316d27441
yutaeho1 <yut02@naver.com>
2026-04-03 11:46:03 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,24 @@
+    #include <stdio.h>
+
+    void print_reverse(int *arr, int size) {
+        int *ptr = arr + size -1;
+
+        for (int i=0; i<size; i++) {
+            if (i == size-1)
+                printf("%d", *ptr);
+            else
+            {
+            printf("%d ", *ptr);
+            ptr--;
+            }
+        }
+    }
+
+    int main() {
+        int nums[] = {1, 2, 3, 4, 5};
+
+        print_reverse(nums, 5);
+        printf("\n");
+
+        return 0;
+    }

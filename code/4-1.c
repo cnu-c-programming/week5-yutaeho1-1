481a0709d406dd03709d8ca8da1f86e8a9d96cd6
yutaeho1 <yut02@naver.com>
2026-04-09 21:13:51 +0900

    Update 4-1.c

---
+++ code/4-1.c
@@ -0,0 +1,16 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    void echo() {
+        char buf[5];
+        scanf("%4s", buf);
+        printf("%s\n", buf);
+        
+    }
+
+    int main() {
+        int x = 42;
+        echo();
+
+        return 0;
+    }

805b22df272de636a3224f899214fc44e90743fa
yutaeho1 <yut02@naver.com>
2026-04-09 21:21:56 +0900

    Update 4-2.c

---
+++ code/4-2.c
@@ -0,0 +1,16 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    void echo() {
+        char buf[5];
+        fgets(buf, sizeof(buf), stdin);
+        printf("%s\n", buf);
+        
+    }
+
+    int main() {
+        int x = 42;
+        echo();
+
+        return 0;
+    }
